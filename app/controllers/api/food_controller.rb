f6886b512b599ce2ec2eb9b0fe558be3c99bf0dd
brittani <britt.sentiere@gmail.com>
2019-09-27 11:07:51 -0500

    removed original API_KEY

--- app/controllers/api/food_controller.rb
+++ app/controllers/api/food_controller.rb
@@ -1,6 +1,6 @@
 class Api::FoodController < ApplicationController
   def index
-    response = HTTP.get("https://www.food2fork.com/api/search?key=f7cad8508326076cad8c94a1c645c981&q=#{params[:search]}")
+    response = HTTP.get("https://www.food2fork.com/api/search?q=#{params[:search]}")
     @food = response.parse["food"]
     render json: @search
   end
